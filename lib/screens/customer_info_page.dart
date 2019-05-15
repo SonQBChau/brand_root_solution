cf82ef6e472a4b269cdc2e899083b1b6fa81a0e9
Son Chau <soncqb@gmail.com>
2019-05-15 10:02:11 -0500

    customized border textfield

--- lib/screens/customer_info_page.dart
+++ lib/screens/customer_info_page.dart
@@ -11,6 +11,93 @@ class CustomerInfoPage extends StatelessWidget {
       ),
       body: Container(
         decoration: BoxDecoration(color: colorGrey10),
+        child: ListView(
+          children: _buildFormWidgets(),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildFormWidgets() {
+    List<Widget> formWidget = new List();
+
+    formWidget.add(ClientName());
+    formWidget.add(CompanyName());
+
+
+    return formWidget;
+  }
+}
+
+class ClientName extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 40, top: 20, bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('NAME', style: TextStyle(color: colorGrey, fontWeight: FontWeight.bold),),
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: colorGrey20,
+                  blurRadius: 2.0, // has the effect of softening the shadow
+                  spreadRadius: 1.0, // has the effect of extending the shadow
+                )
+              ],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                enabledBorder:  OutlineInputBorder(
+                  borderSide:  BorderSide(color: colorGrey20),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                focusedBorder:  OutlineInputBorder(
+                  borderSide:  BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ) ,
+
+                  hintText: 'Client Name',
+                fillColor: Colors.white,
+                filled: true,
+
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CompanyName extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 40, top: 20, bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('COMPANY', style: TextStyle(color: colorGrey, fontWeight: FontWeight.bold),),
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey,
+                  width: 0.5,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                gapPadding: 4,
+              ),
+              hintText: 'Company Name',
+              fillColor: Colors.white,
+              filled: true,
+            ),
+          ),
+        ],
       ),
     );
   }
