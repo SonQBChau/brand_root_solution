999a7cbba771611ae7b0a052d5563df1148ab6b7
Son Chau <soncqb@gmail.com>
2019-05-15 10:40:07 -0500

    add industry widget

--- lib/screens/customer_info_page.dart
+++ lib/screens/customer_info_page.dart
@@ -24,15 +24,16 @@ class CustomerInfoPage extends StatelessWidget {
   List<Widget> _buildFormWidgets() {
     List<Widget> formWidget = new List();
 
-    formWidget.add(ClientName());
-    formWidget.add(CompanyName());
-    formWidget.add(Email());
+    formWidget.add(ClientNameWidget());
+    formWidget.add(CompanyNameWidget());
+    formWidget.add(EmailWidget());
+    formWidget.add(IndustryWidget());
 
     return formWidget;
   }
 }
 
-class ClientName extends StatelessWidget {
+class ClientNameWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -89,7 +90,7 @@ class ClientName extends StatelessWidget {
   }
 }
 
-class CompanyName extends StatelessWidget {
+class CompanyNameWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -146,7 +147,7 @@ class CompanyName extends StatelessWidget {
   }
 }
 
-class Email extends StatelessWidget {
+class EmailWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -194,6 +195,66 @@ class Email extends StatelessWidget {
                 filled: true,
               ),
             ),
+          ),
+          SizedBox(height: 5,),
+          Divider(height: 30,),
+        ],
+      ),
+    );
+  }
+}
+
+class IndustryWidget extends StatelessWidget {
+
+  List<DropdownMenuItem<int>> industryList = [
+    DropdownMenuItem(
+      child: new Text('Refining'),
+      value: 0,
+    ),
+    DropdownMenuItem(
+      child: new Text('Drilling'),
+      value: 0,
+    ),
+    DropdownMenuItem(
+      child: new Text('Oiling'),
+      value: 0,
+    ),
+
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 40, top: 0, bottom: 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'INDUSTRY',
+            style: TextStyle(
+                color: colorGrey, fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: colorGrey20,
+                  blurRadius: 2.0, // has the effect of softening the shadow
+                  spreadRadius: 1.0, // has the effect of extending the shadow
+                )
+              ],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: DropdownButton(
+                hint: new Text('Select Industry'),
+                items: industryList,
+                onChanged: (value){
+                  print(value);
+                }),
           ),
           SizedBox(height: 5,),
           Divider(height: 30,),
