1595e062d0a972833f1d9380ad964c5078106bc9
Son Chau <soncqb@gmail.com>
2019-05-20 16:26:49 -0500

    Update strategies_page.dart

--- lib/screens/strategies_page.dart
+++ lib/screens/strategies_page.dart
@@ -1,17 +1,14 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:sale_form_demo/utils/app_color.dart';
 
 class StrategiesPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
     final double cardContainerHeight = height - 60;
-    final double cardHeight = cardContainerHeight /4;
-    final double cardPosition = cardHeight -20;
+    final double cardHeight = cardContainerHeight / 4;
+    final double cardPosition = cardHeight - 20;
 
     return Scaffold(
       body: SafeArea(
@@ -44,10 +41,48 @@ class StrategiesPage extends StatelessWidget {
                     )
                   ],
                 ),
-
                 child: Text(
                   'STRATEGIES',
                   style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w700),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 70),
+                child: Container(
+                  width: 250,
+                  height: 350,
+                  padding: EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+                  decoration: new BoxDecoration(
+                    color: Colors.greenAccent[100],
+                    borderRadius: new BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black54,
+                        offset: new Offset(0.0, 4.0),
+                        blurRadius: 10.0,
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text('MI STRATEGY MANAGEMENT',
+                      style: TextStyle(
+                        color: colorGreen,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      ),
+                      Text('Managing the risk based, proactive approach and generating the equipment plans/ strengths'
+                          ' that aim to prevent loss of containment events for all plant equipments.',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ],
