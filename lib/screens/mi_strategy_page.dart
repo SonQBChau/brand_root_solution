16687d184a0b694dee7a8400441d3f96218ad79b
Son Chau <soncqb@gmail.com>
2019-05-21 11:13:50 -0500

    Update mi_strategy_page.dart

--- lib/screens/mi_strategy_page.dart
+++ lib/screens/mi_strategy_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sale_form_demo/utils/app_color.dart';
 
 
 class MiStrategyPage extends StatelessWidget {
@@ -6,17 +7,37 @@ class MiStrategyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        constraints: new BoxConstraints.expand(
+        constraints: BoxConstraints.expand(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
         ),
         decoration: BoxDecoration(
+          
           image: new DecorationImage(
             image: new AssetImage("images/Background.png"),
             fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(colorGreen10, BlendMode.color)
           ),
         ),
-
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top:40, left: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.arrow_back, color: Colors.white, size: 50,),
+                SizedBox(height: 10,),
+                Text('MI STRATEGY MANAGEMENT', style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w900,
+                ),),
+              ],
+            ),
+          ),
+        ],
+      ),
       ),
     );
   }
