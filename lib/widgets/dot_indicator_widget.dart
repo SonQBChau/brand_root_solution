2177eca817d2d94392cca419f075c6bddc2e81de
Son Chau <soncqb@gmail.com>
2019-05-21 14:15:55 -0500

    Update dot_indicator_widget.dart

--- lib/widgets/dot_indicator_widget.dart
+++ lib/widgets/dot_indicator_widget.dart
@@ -15,80 +15,128 @@ class DotIndicatorWidget extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 20),
       height: 30,
       width: 260,
-      child: _buildDotIndicator(),
+      child: _buildDotIndicator(dotCount, activePosition, 230),
     );
   }
 
-  _buildDotIndicator() {
+  _buildDotIndicator(int dotCount, int activePosition, double width) {
+    double spacing = width/(dotCount-1);
+    double dotSize = 20;
+    double activeDotSize = 30;
+
+    List<Positioned> dotList = [];
+    Positioned lineWidget =  Positioned(
+      top: 15,
+      left: 5,
+      child: SizedBox(
+        height: 2.0,
+        width: width,
+        child: Container(
+          height: 2.0,
+          color: colorGreen,
+        ),
+      ),
+    );
+
+    Positioned activeDot = Positioned(
+      top: 0,
+      left: 0,
+      child: Container(
+        height: activeDotSize,
+        width: activeDotSize,
+        decoration: new BoxDecoration(shape: BoxShape.circle, color: Colors.green[200]),
+      ),
+    );
+
+    dotList.add(lineWidget);
+    dotList.add(activeDot);
+
+
+
+    for (var i = 0; i < dotCount; i++){
+        Positioned newDot =  Positioned(
+          top: 5,
+          left: 5 + i * spacing,
+          child: Container(
+            height: dotSize,
+            width: dotSize,
+            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
+          ),
+        );
+
+        dotList.add(newDot);
+    }
+
     return Stack(
-      children: <Widget>[
-        Positioned(
-          top: 15,
-          left: 5,
-          child: SizedBox(
-            height: 2.0,
-            width: 260,
-            child: Container(
-              height: 2.0,
-              color: colorGreen,
-            ),
-          ),
-        ),
-        Positioned(
-          top: 0,
-          left: (0),
-          child: Container(
-            height: 30,
-            width: 30,
-            decoration: new BoxDecoration(shape: BoxShape.circle, color: Colors.green[200]),
-          ),
-        ),
-        Positioned(
-          top: 5,
-          left: 5,
-          child: Container(
-            height: 20,
-            width: 20,
-            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
-          ),
-        ),
-        Positioned(
-          top: 5,
-          left: 55,
-          child: Container(
-            height: 20,
-            width: 20,
-            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
-          ),
-        ),
-        Positioned(
-          top: 5,
-          left: 105,
-          child: Container(
-            height: 20,
-            width: 20,
-            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
-          ),
-        ),
-        Positioned(
-          top: 5,
-          left: 155,
-          child: Container(
-            height: 20,
-            width: 20,
-            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
-          ),
-        ),
-        Positioned(
-          top: 5,
-          left: 205,
-          child: Container(
-            height: 20,
-            width: 20,
-            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
-          ),
-        ),
-      ],
+      children: dotList,
+//      <Widget>[
+//        Positioned(
+//          top: 15,
+//          left: 5,
+//          child: SizedBox(
+//            height: 2.0,
+//            width: 260,
+//            child: Container(
+//              height: 2.0,
+//              color: colorGreen,
+//            ),
+//          ),
+//        ),
+//        Positioned(
+//          top: 0,
+//          left: (0),
+//          child: Container(
+//            height: 30,
+//            width: 30,
+//            decoration: new BoxDecoration(shape: BoxShape.circle, color: Colors.green[200]),
+//          ),
+//        ),
+//        Positioned(
+//          top: 5,
+//          left: 5,
+//          child: Container(
+//            height: 20,
+//            width: 20,
+//            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
+//          ),
+//        ),
+//        Positioned(
+//          top: 5,
+//          left: 55,
+//          child: Container(
+//            height: 20,
+//            width: 20,
+//            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
+//          ),
+//        ),
+//        Positioned(
+//          top: 5,
+//          left: 105,
+//          child: Container(
+//            height: 20,
+//            width: 20,
+//            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
+//          ),
+//        ),
+//        Positioned(
+//          top: 5,
+//          left: 155,
+//          child: Container(
+//            height: 20,
+//            width: 20,
+//            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
+//          ),
+//        ),
+//        Positioned(
+//          top: 5,
+//          left: 205,
+//          child: Container(
+//            height: 20,
+//            width: 20,
+//            decoration: new BoxDecoration(shape: BoxShape.circle, color: colorGreen),
+//          ),
+//        ),
+//      ],
     );
   }
 }
