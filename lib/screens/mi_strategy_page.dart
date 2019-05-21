e533e6dd43512c5492d69223772c7efc3815d4b1
Son Chau <soncqb@gmail.com>
2019-05-21 11:27:57 -0500

    add inkwell to back button

--- lib/screens/mi_strategy_page.dart
+++ lib/screens/mi_strategy_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sale_form_demo/utils/app_color.dart';
 
-
 class MiStrategyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,32 +11,49 @@ class MiStrategyPage extends StatelessWidget {
           height: MediaQuery.of(context).size.height,
         ),
         decoration: BoxDecoration(
-          
           image: new DecorationImage(
-            image: new AssetImage("images/Background.png"),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(colorGreen10, BlendMode.color)
-          ),
+              image: new AssetImage("images/Background.png"),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(colorGreen10, BlendMode.color)),
         ),
-      child: Stack(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(top:40, left: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Icon(Icons.arrow_back, color: Colors.white, size: 50,),
-                SizedBox(height: 10,),
-                Text('MI STRATEGY MANAGEMENT', style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w900,
-                ),),
-              ],
+        child: Stack(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: 40, left: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Material(
+
+                    color: Colors.transparent,
+                    child: InkWell(
+                        borderRadius: BorderRadius.circular(30),
+                        onTap: (){
+
+                        },
+                        child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                      size: 50,
+                    )),
+                  ),
+
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'MI STRATEGY MANAGEMENT',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
       ),
     );
   }
