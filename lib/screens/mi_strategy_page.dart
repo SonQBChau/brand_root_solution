db1ec5ceb3b4d23cc266c47fd50e50f9147dd5ef
Son Chau <soncqb@gmail.com>
2019-05-30 09:22:17 -0500

    refactor build question list

--- lib/screens/mi_strategy_page.dart
+++ lib/screens/mi_strategy_page.dart
@@ -3,6 +3,7 @@ import 'package:sale_form_demo/data/question_model.dart';
 import 'package:sale_form_demo/utils/app_color.dart';
 import 'package:sale_form_demo/utils/size_config.dart';
 import 'package:sale_form_demo/widgets/checkbox_widget.dart';
+import 'package:sale_form_demo/widgets/question_list_widget.dart';
 import 'package:sale_form_demo/widgets/tab_header_widget.dart';
 
 
@@ -37,6 +38,7 @@ class _MiStrategyPageState extends State<MiStrategyPage> {
     SizeConfig().init(context);
     final double screeHeight = SizeConfig.safeAreaScreenHeight;
     final double screenWidth = SizeConfig.safeAreaScreenWidth;
+
 
     return Scaffold(
       body: Container(
@@ -105,7 +107,7 @@ class _MiStrategyPageState extends State<MiStrategyPage> {
                       child: Form(
                         key: _formKey,
                         child: ListView(
-                          children: _buildFormWidgets(),
+                          children: buildFormWidgets(questionCenter, _activeHeader, updateCheckboxValue),
                         ),
                       ),
                     ),
@@ -118,183 +120,5 @@ class _MiStrategyPageState extends State<MiStrategyPage> {
       ),
     );
   }
-
-  List<Widget> _buildFormWidgets() {
-    List<Widget> formWidget;
-    List<Widget> formWidget1 = new List();
-    List<Widget> formWidget2 = new List();
-    List<Widget> formWidget3 = new List();
-
-    //form 1
-    formWidget1.add(CheckboxWidget(
-      label: questionCenter.questionBank[0].getLabel(),
-      widgetValue: questionCenter.questionBank[0].getValue(),
-      index: 0,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget1.add(CheckboxWidget(
-      label: questionCenter.questionBank[1].getLabel(),
-      widgetValue: questionCenter.questionBank[1].getValue(),
-      index: 1,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget1.add(CheckboxWidget(
-      label: questionCenter.questionBank[2].getLabel(),
-      widgetValue: questionCenter.questionBank[2].getValue(),
-      index: 2,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget1.add(CheckboxWidget(
-      label: questionCenter.questionBank[3].getLabel(),
-      widgetValue: questionCenter.questionBank[3].getValue(),
-      index: 3,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget1.add(CheckboxWidget(
-      label: questionCenter.questionBank[4].getLabel(),
-      widgetValue: questionCenter.questionBank[4].getValue(),
-      index: 4,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget1.add(CheckboxWidget(
-      label: questionCenter.questionBank[5].getLabel(),
-      widgetValue: questionCenter.questionBank[5].getValue(),
-      index: 5,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget1.add(CheckboxWidget(
-      label: questionCenter.questionBank[6].getLabel(),
-      widgetValue: questionCenter.questionBank[6].getValue(),
-      index: 6,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget1.add(CheckboxWidget(
-      label: questionCenter.questionBank[7].getLabel(),
-      widgetValue: questionCenter.questionBank[7].getValue(),
-      index: 7,
-      notifyParent: updateCheckboxValue,
-    ));
-
-    //form 2
-    formWidget2.add(CheckboxWidget(
-      label: questionCenter.questionBank[8].getLabel(),
-      widgetValue: questionCenter.questionBank[8].getValue(),
-      index: 8,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget2.add(CheckboxWidget(
-      label: questionCenter.questionBank[9].getLabel(),
-      widgetValue: questionCenter.questionBank[9].getValue(),
-      index: 9,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget2.add(CheckboxWidget(
-      label: questionCenter.questionBank[10].getLabel(),
-      widgetValue: questionCenter.questionBank[10].getValue(),
-      index: 10,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget2.add(CheckboxWidget(
-      label: questionCenter.questionBank[11].getLabel(),
-      widgetValue: questionCenter.questionBank[11].getValue(),
-      index: 11,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget2.add(CheckboxWidget(
-      label: questionCenter.questionBank[12].getLabel(),
-      widgetValue: questionCenter.questionBank[12].getValue(),
-      index: 12,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget2.add(CheckboxWidget(
-      label: questionCenter.questionBank[13].getLabel(),
-      widgetValue: questionCenter.questionBank[13].getValue(),
-      index: 13,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget2.add(CheckboxWidget(
-      label: questionCenter.questionBank[14].getLabel(),
-      widgetValue: questionCenter.questionBank[14].getValue(),
-      index: 14,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget2.add(CheckboxWidget(
-      label: questionCenter.questionBank[15].getLabel(),
-      widgetValue: questionCenter.questionBank[15].getValue(),
-      index: 15,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget2.add(CheckboxWidget(
-      label: questionCenter.questionBank[16].getLabel(),
-      widgetValue: questionCenter.questionBank[16].getValue(),
-      index: 16,
-      notifyParent: updateCheckboxValue,
-    ));
-
-    //form 3
-    formWidget3.add(CheckboxWidget(
-      label: questionCenter.questionBank[17].getLabel(),
-      widgetValue: questionCenter.questionBank[17].getValue(),
-      index: 17,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget3.add(CheckboxWidget(
-      label: questionCenter.questionBank[18].getLabel(),
-      widgetValue: questionCenter.questionBank[18].getValue(),
-      index: 18,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget3.add(CheckboxWidget(
-      label: questionCenter.questionBank[19].getLabel(),
-      widgetValue: questionCenter.questionBank[19].getValue(),
-      index: 19,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget3.add(CheckboxWidget(
-      label: questionCenter.questionBank[20].getLabel(),
-      widgetValue: questionCenter.questionBank[20].getValue(),
-      index: 20,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget3.add(CheckboxWidget(
-      label: questionCenter.questionBank[21].getLabel(),
-      widgetValue: questionCenter.questionBank[21].getValue(),
-      index: 21,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget3.add(CheckboxWidget(
-      label: questionCenter.questionBank[22].getLabel(),
-      widgetValue: questionCenter.questionBank[22].getValue(),
-      index: 22,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget3.add(CheckboxWidget(
-      label: questionCenter.questionBank[23].getLabel(),
-      widgetValue: questionCenter.questionBank[23].getValue(),
-      index: 23,
-      notifyParent: updateCheckboxValue,
-    ));
-    formWidget3.add(CheckboxWidget(
-      label: questionCenter.questionBank[24].getLabel(),
-      widgetValue: questionCenter.questionBank[24].getValue(),
-      index: 24,
-      notifyParent: updateCheckboxValue,
-    ));
-
-
-
-
-    // present the list of questionares based on active tab
-    if (_activeHeader == 'High Impact') {
-      formWidget = formWidget1;
-    } else if (_activeHeader == 'Medium Impact') {
-      formWidget = formWidget2;
-    } else if (_activeHeader == 'Low Impact') {
-      formWidget = formWidget3;
-    }
-
-    return formWidget;
-  }
-
 
 }
---
+++ lib/widgets/question_list_widget.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:sale_form_demo/widgets/checkbox_widget.dart';
+
+List<Widget> buildFormWidgets(questionCenter, activeHeader, updateCheckboxValue) {
+  List<Widget> formWidget;
+  List<Widget> formWidget1 = new List();
+  List<Widget> formWidget2 = new List();
+  List<Widget> formWidget3 = new List();
+
+  //form 1
+  formWidget1.add(CheckboxWidget(
+    label: questionCenter.questionBank[0].getLabel(),
+    widgetValue: questionCenter.questionBank[0].getValue(),
+    index: 0,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget1.add(CheckboxWidget(
+    label: questionCenter.questionBank[1].getLabel(),
+    widgetValue: questionCenter.questionBank[1].getValue(),
+    index: 1,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget1.add(CheckboxWidget(
+    label: questionCenter.questionBank[2].getLabel(),
+    widgetValue: questionCenter.questionBank[2].getValue(),
+    index: 2,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget1.add(CheckboxWidget(
+    label: questionCenter.questionBank[3].getLabel(),
+    widgetValue: questionCenter.questionBank[3].getValue(),
+    index: 3,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget1.add(CheckboxWidget(
+    label: questionCenter.questionBank[4].getLabel(),
+    widgetValue: questionCenter.questionBank[4].getValue(),
+    index: 4,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget1.add(CheckboxWidget(
+    label: questionCenter.questionBank[5].getLabel(),
+    widgetValue: questionCenter.questionBank[5].getValue(),
+    index: 5,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget1.add(CheckboxWidget(
+    label: questionCenter.questionBank[6].getLabel(),
+    widgetValue: questionCenter.questionBank[6].getValue(),
+    index: 6,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget1.add(CheckboxWidget(
+    label: questionCenter.questionBank[7].getLabel(),
+    widgetValue: questionCenter.questionBank[7].getValue(),
+    index: 7,
+    notifyParent: updateCheckboxValue,
+  ));
+
+  //form 2
+  formWidget2.add(CheckboxWidget(
+    label: questionCenter.questionBank[8].getLabel(),
+    widgetValue: questionCenter.questionBank[8].getValue(),
+    index: 8,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget2.add(CheckboxWidget(
+    label: questionCenter.questionBank[9].getLabel(),
+    widgetValue: questionCenter.questionBank[9].getValue(),
+    index: 9,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget2.add(CheckboxWidget(
+    label: questionCenter.questionBank[10].getLabel(),
+    widgetValue: questionCenter.questionBank[10].getValue(),
+    index: 10,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget2.add(CheckboxWidget(
+    label: questionCenter.questionBank[11].getLabel(),
+    widgetValue: questionCenter.questionBank[11].getValue(),
+    index: 11,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget2.add(CheckboxWidget(
+    label: questionCenter.questionBank[12].getLabel(),
+    widgetValue: questionCenter.questionBank[12].getValue(),
+    index: 12,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget2.add(CheckboxWidget(
+    label: questionCenter.questionBank[13].getLabel(),
+    widgetValue: questionCenter.questionBank[13].getValue(),
+    index: 13,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget2.add(CheckboxWidget(
+    label: questionCenter.questionBank[14].getLabel(),
+    widgetValue: questionCenter.questionBank[14].getValue(),
+    index: 14,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget2.add(CheckboxWidget(
+    label: questionCenter.questionBank[15].getLabel(),
+    widgetValue: questionCenter.questionBank[15].getValue(),
+    index: 15,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget2.add(CheckboxWidget(
+    label: questionCenter.questionBank[16].getLabel(),
+    widgetValue: questionCenter.questionBank[16].getValue(),
+    index: 16,
+    notifyParent: updateCheckboxValue,
+  ));
+
+  //form 3
+  formWidget3.add(CheckboxWidget(
+    label: questionCenter.questionBank[17].getLabel(),
+    widgetValue: questionCenter.questionBank[17].getValue(),
+    index: 17,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget3.add(CheckboxWidget(
+    label: questionCenter.questionBank[18].getLabel(),
+    widgetValue: questionCenter.questionBank[18].getValue(),
+    index: 18,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget3.add(CheckboxWidget(
+    label: questionCenter.questionBank[19].getLabel(),
+    widgetValue: questionCenter.questionBank[19].getValue(),
+    index: 19,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget3.add(CheckboxWidget(
+    label: questionCenter.questionBank[20].getLabel(),
+    widgetValue: questionCenter.questionBank[20].getValue(),
+    index: 20,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget3.add(CheckboxWidget(
+    label: questionCenter.questionBank[21].getLabel(),
+    widgetValue: questionCenter.questionBank[21].getValue(),
+    index: 21,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget3.add(CheckboxWidget(
+    label: questionCenter.questionBank[22].getLabel(),
+    widgetValue: questionCenter.questionBank[22].getValue(),
+    index: 22,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget3.add(CheckboxWidget(
+    label: questionCenter.questionBank[23].getLabel(),
+    widgetValue: questionCenter.questionBank[23].getValue(),
+    index: 23,
+    notifyParent: updateCheckboxValue,
+  ));
+  formWidget3.add(CheckboxWidget(
+    label: questionCenter.questionBank[24].getLabel(),
+    widgetValue: questionCenter.questionBank[24].getValue(),
+    index: 24,
+    notifyParent: updateCheckboxValue,
+  ));
+
+
+
+
+  // present the list of questionares based on active tab
+  if (activeHeader == 'High Impact') {
+    formWidget = formWidget1;
+  } else if (activeHeader == 'Medium Impact') {
+    formWidget = formWidget2;
+  } else if (activeHeader == 'Low Impact') {
+    formWidget = formWidget3;
+  }
+
+  return formWidget;
+}
