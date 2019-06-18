f2c5dbe110670569e4fc03e0300097e7486871e3
Son Chau <soncqb@gmail.com>
2019-06-18 13:59:42 -0500

    Update menu_page.dart

--- lib/screens/menu_page.dart
+++ lib/screens/menu_page.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:sale_form_demo/utils/app_color.dart';
 import 'package:sale_form_demo/widgets/company_full_logo.dart';
 
 class MenuPage extends StatelessWidget {
@@ -23,6 +24,38 @@ class MenuPage extends StatelessWidget {
               SizedBox(height: 50,),
               CompanyFullLogo(),
               SizedBox(height: 50,),
+              
+              
+              
+              GestureDetector(
+                onTap: () {
+
+                },
+                child: Container(
+                  width: 300,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+                  decoration: new BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: new BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black54,
+                        offset: new Offset(0.0, 4.0),
+                        blurRadius: 10.0,
+                      )
+                    ],
+                  ),
+                  child: Text('EVALUATE',
+                  style: TextStyle(
+                    color: colorBlue,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  ),
+                ),
+              ),
+              
 
           ],
         ),
