18e6d17e9dca760bfc0bc82113d8163c6eddb4b5
Son Chau <soncqb@gmail.com>
2019-06-19 10:18:54 -0500

    add bottom sheet

--- lib/screens/mi_strategy_page.dart
+++ lib/screens/mi_strategy_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sale_form_demo/utils/app_color.dart';
+import 'package:sale_form_demo/widgets/mi_strategy_bottom_sheet.dart';
 
 class MiStrategyPage extends StatelessWidget {
 
@@ -14,66 +15,73 @@ class MiStrategyPage extends StatelessWidget {
           height: screenHeight,
         ),
         color: colorBlue,
-        child: Column(
+        child: Stack(
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(left: 30, top: 60),
-              child: Text(
-                'MI Strategy Management',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 30, top: 20, right: 20, bottom: 30),
-              child: Text(
-                'Managing the risk based, proactive approach and generating the equipment plans/ strengths'
-                ' that aim to prevent loss of containment events for all plant equipments.',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                ),
-              ),
-            ),
-            Center(
-              child: Card(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-                child: InkWell(
-                  splashColor: Colors.blue.withAlpha(30),
-                  onTap: () {
-                    print('Card tapped.');
-                  },
-                  child: Container(
-                    width: screenWidth - 20,
-                    height: 160,
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                    color: colorBlue80,
-                    child: Text('A card that can be tapped'),
+            Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(left: 30, top: 60),
+                  child: Text(
+                    'MI Strategy Management',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Center(
-              child: Card(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-                child: InkWell(
-                  splashColor: Colors.blue.withAlpha(30),
-                  onTap: () {
-                    print('Card tapped.');
-                  },
-                  child: Container(
-                    width: screenWidth - 20,
-                    height: 160,
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                    color: colorBlue80,
-                    child: Text('A card that can be tapped'),
+                Container(
+                  padding: EdgeInsets.only(left: 30, top: 20, right: 20, bottom: 30),
+                  child: Text(
+                    'Managing the risk based, proactive approach and generating the equipment plans/ strengths'
+                    ' that aim to prevent loss of containment events for all plant equipments.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
                   ),
                 ),
-              ),
+                Center(
+                  child: Card(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {
+                        print('Card tapped.');
+                      },
+                      child: Container(
+                        width: screenWidth - 20,
+                        height: 160,
+                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        color: colorBlue80,
+                        child: Text('A card that can be tapped'),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Card(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {
+                        print('Card tapped.');
+                      },
+                      child: Container(
+                        width: screenWidth - 20,
+                        height: 160,
+                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        color: colorBlue80,
+                        child: Text('A card that can be tapped'),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
+
+
+            MIStrategyBottomSheet(),
           ],
         ),
       ),
