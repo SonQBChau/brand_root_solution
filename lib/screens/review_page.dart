0fefe3943148f10578c9e961c72581844fe173fa
Son Chau <soncqb@gmail.com>
2019-06-21 11:38:10 -0500

    tab bar widget without scaffold

--- lib/screens/review_page.dart
+++ lib/screens/review_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:sale_form_demo/utils/app_color.dart';
 import 'package:sale_form_demo/widgets/company_full_logo.dart';
+import 'package:sale_form_demo/widgets/review_tab_container_widget.dart';
 
 class ReviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -24,29 +24,46 @@ class ReviewPage extends StatelessWidget {
         children: <Widget>[
           CompanyFullLogo(),
           SizedBox(height: 30),
-          Expanded(
-            child: Container(
-              decoration:  BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:  BorderRadius.only(
-                      topLeft: const Radius.circular(40.0),
-                      topRight: const Radius.circular(40.0)
-                  ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 5.0, // has the effect of softening the shadow
-                    spreadRadius: 3.0, // has the effect of extending the shadow
-                    offset: Offset(
-                      0.0, // horizontal, move right 10
-                      2.0, // vertical, move down 10
-                    ),
-                  )
-                ],
-              ),
-
-            ),
-          ),
+          ReviewTabContainerWidget(),
+//          Expanded(
+//            child: Container(
+//              padding: EdgeInsets.only(top: 32),
+//              decoration: BoxDecoration(
+//                color: Colors.white,
+//                borderRadius:
+//                    BorderRadius.only(topLeft: const Radius.circular(40.0), topRight: const Radius.circular(40.0)),
+//                boxShadow: [
+//                  BoxShadow(
+//                    color: Colors.grey,
+//                    blurRadius: 5.0, // has the effect of softening the shadow
+//                    spreadRadius: 3.0, // has the effect of extending the shadow
+//                    offset: Offset(
+//                      0.0, // horizontal, move right 10
+//                      2.0, // vertical, move down 10
+//                    ),
+//                  )
+//                ],
+//              ),
+//              child: DefaultTabController(
+//                length: 2,
+//                child: Scaffold(
+//                  appBar: PreferredSize(
+//                    preferredSize: Size.fromHeight(35.0),
+//                    child: TabBar(
+//
+//                      labelStyle: TextStyle(color: Colors.white),
+//
+//                      tabs: <Widget>[
+//                        Tab(child: Text('EVALUATE', style: TextStyle(fontSize: 18.0,color:colorBlue))),
+//                        Tab(child: Text('STRATEGIES', style: TextStyle(fontSize: 18.0, color: colorBlue))),
+//                      ],
+//
+//                    ),
+//                  ),
+//                ),
+//              ),
+//            ),
+//          ),
         ],
       ),
     );
---
+++ lib/widgets/review_tab_container_widget.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:sale_form_demo/utils/app_color.dart';
+
+class ReviewTabContainerWidget extends StatefulWidget {
+  ReviewTabContainerWidget({Key key}) : super(key: key);
+
+  @override
+  _ReviewTabContainerWidgetState createState() => _ReviewTabContainerWidgetState();
+}
+
+class _ReviewTabContainerWidgetState extends State<ReviewTabContainerWidget> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: 2,
+      vsync: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(topLeft: const Radius.circular(40.0), topRight: const Radius.circular(40.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 5.0, // has the effect of softening the shadow
+              spreadRadius: 3.0, // has the effect of extending the shadow
+              offset: Offset(
+                0.0, // horizontal, move right 10
+                2.0, // vertical, move down 10
+              ),
+            )
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top:20, left: 20),
+              child: Text('SOLUTION CATEGORIES',
+              style: TextStyle(color: colorBlue, fontWeight: FontWeight.w700, fontSize: 12),
+              ),
+            ),
+            TabBar(
+              labelColor: Colors.blue,
+              controller: _tabController,
+              tabs: <Widget>[
+                Tab(
+                  icon: const Icon(Icons.home),
+                  text: 'Address',
+                ),
+                Tab(
+                  icon: const Icon(Icons.my_location),
+                  text: 'Location',
+                ),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: <Widget>[
+                  Center(
+                    child: Text('Address'),
+                  ),
+                  Center(
+                    child: Text('Location'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
