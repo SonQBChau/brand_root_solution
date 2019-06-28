a5097ccf3b86dcbd88989d60240f1afbe2cd4f37
Son Chau <soncqb@gmail.com>
2019-06-28 11:40:47 -0500

    Update custom_page_transition.dart

--- lib/utils/custom_page_transition.dart
+++ lib/utils/custom_page_transition.dart
@@ -7,11 +7,8 @@ class CustomPageTransition extends PageRouteBuilder {
         return widget;
       },
       transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
-        return SlideTransition(
-          position: new Tween<Offset>(
-            begin: const Offset(0.0, 1.0),
-            end: Offset.zero,
-          ).animate(animation),
+        return FadeTransition(
+          opacity: animation,
           child: child,
         );
       }
