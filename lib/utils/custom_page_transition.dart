1962915cc5e73e97b4c27bc00d6f1c47201e8680
Son Chau <soncqb@gmail.com>
2019-07-11 17:34:48 -0500

    click logo slide right

--- lib/utils/custom_page_transition.dart
+++ lib/utils/custom_page_transition.dart
@@ -130,3 +130,34 @@ class FadeAndScaleRoute extends PageRouteBuilder {
         ),
   );
 }
+
+class SlideRightRoute extends PageRouteBuilder {
+
+  final Widget page;
+  SlideRightRoute({
+    this.page})
+      :
+        super(
+        pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            ) =>
+        page,
+        transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+            ) =>
+            SlideTransition(
+              position: Tween<Offset>(
+                begin:
+                const Offset(-1, 0),
+                end: Offset.
+                zero,
+              ).animate(animation),
+              child: child,
+            ),
+      );
+}
--- lib/widgets/company_full_logo.dart
+++ lib/widgets/company_full_logo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sale_form_demo/screens/menu_page.dart';
+import 'package:sale_form_demo/utils/custom_page_transition.dart';
 import 'package:sale_form_demo/widgets/company_logo_widget.dart';
 import 'package:sale_form_demo/widgets/company_name_widget.dart';
 
@@ -11,7 +13,8 @@ class CompanyFullLogo extends StatelessWidget {
     return  GestureDetector(
       onLongPress: () {
         if (canPop){
-          Navigator.of(context).pushReplacementNamed('/menu');
+//          Navigator.of(context).pushReplacementNamed('/menu');
+          Navigator.push(context, SlideRightRoute(page: MenuPage()));
         }
       },
       child: Stack(
