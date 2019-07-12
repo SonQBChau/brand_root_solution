b5979bd5e3a5bf592fe5b4d3fa71066447b64d20
Son Chau <soncqb@gmail.com>
2019-07-12 10:35:55 -0500

    status color for sample page

--- lib/screens/sample_delivery_page.dart
+++ lib/screens/sample_delivery_page.dart
@@ -7,6 +7,7 @@ class SampleDeliveryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        brightness: Brightness.light,
         backgroundColor: Colors.white,
         elevation: 0,
         leading:  IconButton(
